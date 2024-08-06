b22d358b1ce2c3fb0acf066606906947fa804acd
prathamodasia <modasiapratha@gmail.com>
2024-08-06 11:17:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import numpy as np
 import pickle
 
 # Load the model
-clf = pickle.load(open("emp_model.pkl","rb"))
+clf = pickle.load(open("case_study_emp.pkl","rb"))
 
 def predict(data):
-    clf = pickle.load(open("emp_model.pkl","rb"))
+    clf = pickle.load(open("case_study_emp.pkl","rb"))
     return clf.predict(data)
 
 
